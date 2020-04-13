002742722d7fd0d87e548bf2f766aa5456330076
Akhil <Akhil@DESKTOP-CUHJILV>
2020-04-13 00:05:57 -0700

    java lab

---
+++ src/java_lab8/Ex1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_lab8;
+
+/**
+ *
+ * @author Akhil
+ */
+import P1.*;
+public class Ex1 {
+    public static void main(String args[])
+    {
+        Ex E = new Ex();
+        E.display();
+    }
+}
