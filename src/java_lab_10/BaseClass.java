3d2e437d61fc7e07b145f7f8f5fc07585218f4d9
Akhil <Akhil@DESKTOP-CUHJILV>
2020-04-13 02:51:53 -0700

    java lab

--- src/java_lab_10/BaseClass.java
+++ src/java_lab_10/BaseClass.java
@@ -1,4 +1,4 @@
-/*
+/*A program to illustrate sub class exception precedence over base class
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
