c28398ebe4f0f3adeb42f1d041cf0f59462e2233
Akhil <Akhil@DESKTOP-CUHJILV>
2020-04-13 23:18:18 -0700

    mypack

---
+++ src/Mypack/Circle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mypack;
+
+/**
+ *
+ * @author Akhil
+ */
+public class Circle {
+  int r;
+public void area()
+{
+    System.out.println("Area of the circle is = " + (3.14*4*4));
+    
+}
+}
